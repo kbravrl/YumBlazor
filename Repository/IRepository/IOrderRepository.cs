53989e5ee630684069bfc32dc02c7752125fe329
kbravrl <kubravarol499@gmail.com>
2026-02-18 14:17:57 +0300

    Order Repository Implementation

--- Repository/IRepository/IOrderRepository.cs
+++ Repository/IRepository/IOrderRepository.cs
@@ -4,7 +4,7 @@ namespace YumBlazor.Repository.IRepository
 {
     public interface IOrderRepository
     {
-        public Task<OrderHeader> CreateAsync(OrderHeader order);
+        public Task<OrderHeader> CreateAsync(OrderHeader orderHeader);
         public Task<OrderHeader> GetAsync(int id);
         public Task<IEnumerable<OrderHeader>> GetAllAsync(string? userId = null);
         public Task<OrderHeader> UpdateStatusAsync(int orderId, string status);
