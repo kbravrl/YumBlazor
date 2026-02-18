8b689a8de3a5b250a94223a06f8d4f63ba8be2c0
kbravrl <kubravarol499@gmail.com>
2026-02-18 13:48:07 +0300

    Order Repository Interface

---
+++ Repository/IRepository/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using YumBlazor.Data;
+
+namespace YumBlazor.Repository.IRepository
+{
+    public interface IOrderRepository
+    {
+        public Task<OrderHeader> CreateAsync(OrderHeader order);
+        public Task<OrderHeader> GetAsync(int id);
+        public Task<IEnumerable<OrderHeader>> GetAllAsync(string? userId = null);
+        public Task<OrderHeader> UpdateStatusAsync(int orderId, string status);
+
+
+    }
+}
